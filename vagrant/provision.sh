8264a23e18e9e4ae1ae16f6c5f6292e1df20ea24
AnimeBytes <staff@animebytes.tv>
2019-12-14 15:32:18 +0100

    sync with 6fd3f454d54a7de07b825bf08c28cd4510a56708

--- vagrant/provision.sh
+++ vagrant/provision.sh
@@ -6,15 +6,19 @@ export DEBIAN_FRONTEND=noninteractive
 echo
 echo Installing required base components
 apt-get update
-apt-get -y --force-yes install apt-transport-https dirmngr curl htop iotop
+apt-get -y install apt-transport-https dirmngr curl htop iotop
 
 echo
 echo Updating apt cache
 apt-get update
 
 echo
+echo Updating currently installed packages
+apt-get -y dist-upgrade
+
+echo
 echo Installing packages...
-apt-get -y --force-yes install software-properties-common nginx php7.3 php7.3-curl php7.3-fpm php7.3-common phpunit pv php7.3-dev php-pear libcurl3-openssl-dev build-essential php7.3-cli git libpq5 libodbc1 unzip zip php7.3-apcu php7.3-json
+apt-get -y install software-properties-common nginx php7.3 php7.3-curl php7.3-fpm php7.3-common phpunit pv php7.3-dev php-pear libcurl3-openssl-dev build-essential php7.3-cli git libpq5 libodbc1 unzip zip php7.3-apcu php7.3-json
 
 pecl install xdebug-2.7.2
 
@@ -25,8 +29,6 @@ rm -f /etc/nginx/sites-enabled/default
 rm -rf /etc/nginx/conf.d
 cd /vagrantroot/configs
 cp -av * /
-# remove xdebug from php-cli (composer performance)
-#rm -f /etc/php/7.3/cli/conf.d/20-xdebug.ini
 
 echo
 echo Installing composer as /usr/local/bin/composer...
