d7a2b2307f9992c65fe94e0d4f98a2d308783859
AnimeBytes <staff@animebytes.tv>
2020-01-28 13:51:29 +0100

    sync with 45dd7ebbad5d66c9b682bfaddf4d3b2cfcb5ecd7

--- src/Status/DependencyInjection.php
+++ src/Status/DependencyInjection.php
@@ -7,7 +7,6 @@ namespace Status;
 use DI;
 use Exception;
 use Psr\Container\ContainerInterface as Container;
-use RunTracy\Helpers\Profiler\Profiler;
 use RunTracy\Helpers\TwigPanel;
 use Slim\Views\Twig;
 use Status\Cache\Apc;
@@ -33,67 +32,49 @@ final class DependencyInjection
     {
         $builder = new DI\ContainerBuilder();
         $builder->useAnnotations(true);
+        if ($config['mode'] === 'production') {
+            $builder->enableCompilation(BASE_ROOT);
+        }
         $builder->addDefinitions(
             [
-                'settings' => [
-                    'xdebugHelperIdeKey' => 'status',
-                ],
-                'config' => $config,
-                'obLevel' => ob_get_level(),
-                IKeyStore::class => DI\autowire(Apc::class)->constructorParameter('keyPrefix', '')
+                // utilities
+                Utilities\Assets::class => DI\autowire(),
+                Utilities\View::class => DI\autowire(),
+                // runtime
+                IKeyStore::class => DI\autowire(Apc::class)->constructorParameter('keyPrefix', ''),
+                Twig::class => function (Container $di) {
+                    $dir = $di->get('config')['templates.path'];
+
+                    $config = [
+                        'cache' => $di->get('config')['templates.cache_path'],
+                        'strict_variables' => true,
+                    ];
+
+                    if ($di->get('config')['mode'] === 'development') {
+                        $config['debug'] = true;
+                        $config['auto_reload'] = true;
+                    }
+
+                    $view = Twig::create($dir, $config);
+
+                    if ($di->get('config')['mode'] === 'development') {
+                        $profiler = new TwigProfile();
+                        $view->addExtension(new ProfilerExtension($profiler));
+                        Debugger::getBar()->addPanel(new TwigPanel($profiler));
+                    }
+
+                    $view->addExtension(new TwigExtension($di->get(Utilities\View::class)));
+
+                    return $view;
+                }
             ]
         );
         $di = $builder->build();
 
-        $di = self::setUtilities($di);
-
-        $di->set(
-            Twig::class,
-            function (Container $di) {
-                $dir = $di->get('config')['templates.path'];
-
-                $config = [
-                    'cache' => $di->get('config')['templates.cache_path'],
-                    'strict_variables' => true,
-                ];
-
-                if ($di->get('config')['mode'] === 'development') {
-                    $config['debug'] = true;
-                    $config['auto_reload'] = true;
-                }
-
-                $view = Twig::create($dir, $config);
-
-                if ($di->get('config')['mode'] === 'development') {
-                    $profiler = new TwigProfile();
-                    $view->addExtension(new ProfilerExtension($profiler));
-                    Debugger::getBar()->addPanel(new TwigPanel($profiler));
-                }
-
-                $view->addExtension(new TwigExtension($di->get(Utilities\View::class)));
-
-                return $view;
-            }
-        );
+        // dynamic definitions
+        $di->set('config', $config);
+        $di->set('obLevel', ob_get_level());
 
         return $di;
     }
-
-    /**
-     * @param Container $di
-     *
-     * @return Container
-     */
-    private static function setUtilities(Container $di) : Container /** @formatter:off */
-    {
-        Profiler::start('setUtilities');
-
-        $di->set(Utilities\Assets::class, DI\autowire());
-        $di->set(Utilities\View::class, DI\autowire());
-
-        Profiler::finish('setUtilities');
-
-        return $di;
-    }
-    /** @formatter:on */
 }
